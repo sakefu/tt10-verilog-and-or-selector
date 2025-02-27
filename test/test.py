d1826774a1f878239874f4eea9b690d1866477b3
sakefu <96125313+sakefu@users.noreply.github.com>
2025-02-26 21:05:38 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -48,7 +48,7 @@ async def test_project(dut):
         print(dut.uo_out.value)
         print("Expected:")
         print(c)
-        assert dut.uo_out.value == c, f"Mismatch! Expected {c:08b}, got {dut.uo_out.value:08b}"
+        assert dut.uo_out.value == c
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
